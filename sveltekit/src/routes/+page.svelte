4e2ab51c9a155a13028aa30bd49f3674b192ba76
aaryan-jangra <22cse019aryan@eitfaridabad.co.in>
2025-10-29 22:09:48 +0530

    Add initial SvelteKit page with click counter

---
+++ sveltekit/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = 0
+</script>
+
+<main style="font-family: system-ui, Arial; padding:2rem;">
+  <h1>SvelteKit Starter</h1>
+  <p>Clicks: {count}</p>
+  <button on:click={() => count++}>Click me</button>
+</main>
