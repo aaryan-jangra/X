c449a4afeff3fdd663f96f6a4e1743aa85e0ac18
aaryan-jangra <22cse019aryan@eitfaridabad.co.in>
2025-10-29 22:05:30 +0530

    Create initial App.vue for Vue 3 + Vite project

---
+++ vue-vite/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    <h1>Vue 3 + Vite</h1>
+    <p>Minimal starter app.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style src="./styles.css"></style>
